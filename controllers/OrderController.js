4a6d2db5295f504d07c9478ac42cae39df528740
duyvo2005 <duyvo1954@gmail.com>
2025-02-08 20:17:34 +0700

    Adding insert, delete, update method for order entity

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -1,4 +1,3 @@
-import { Sequelize } from "sequelize";
 import db from "../models";
 
 export async function getOrders(req, res) {
@@ -14,19 +13,39 @@ export async function getOrderById(req, res) {
 }
 
 export async function insertOrder(req, res) {
-  res.status(200).json({
-    message: "thêm mới đơn hàng thành công!",
+  const order = await db.order.create(req.body);
+  return res.status(201).json({
+    message: "Thêm mới đơn hàng thành công!",
+    data: order,
   });
 }
 
 export async function deleteOrder(req, res) {
-  res.status(200).json({
-    message: "xóa đơn hàng thành công!",
+  const { id } = req.params;
+  const deleted = await db.order.destroy({
+    where: { id },
   });
+
+  return deleted
+    ? res.status(200).json({
+        message: "Xóa đơn hàng thành công!",
+      })
+    : res.status(404).json({
+        message: "Không tìm thấy đơn hàng với ID này",
+      });
 }
 
 export async function updateOrder(req, res) {
-  res.status(200).json({
-    message: "cập nhật đơn hàng thành công!",
+  const { id } = req.params;
+  const updatedOrder = await db.order.update(req.body, {
+    where: { id },
   });
+
+  return updatedOrder[0] > 0
+    ? res.status(200).json({
+        message: "Cập nhật đơn hàng thành công!",
+      })
+    : res.status(404).json({
+        message: "Đơn hàng không tìm thấy!",
+      });
 }
