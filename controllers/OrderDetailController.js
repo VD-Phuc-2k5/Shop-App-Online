fb49f06352a1d3ed564c3f4cc10807bd3e75a212
duyvo2005 <duyvo1954@gmail.com>
2025-02-08 20:25:40 +0700

    Adding insert, delete, get, update for order detail entity

--- controllers/OrderDetailController.js
+++ controllers/OrderDetailController.js
@@ -2,31 +2,61 @@ import { Sequelize } from "sequelize";
 import db from "../models";
 
 export async function getOrderDetails(req, res) {
-  res.status(200).json({
-    message: "lấy danh sách chi tiết đơn hàng thành công!",
+  const orderDetails = await db.orderDetail.findAll();
+  return res.status(200).json({
+    message: "Lấy danh sách chi tiết đơn hàng thành công!",
+    data: orderDetails,
   });
 }
 
 export async function getOrderDetailById(req, res) {
-  res.status(200).json({
-    message: "lấy thông tin chi tiết đơn hàng",
-  });
+  const { id } = req.params;
+  const orderDetail = await db.orderDetail.findOne({ where: { id } });
+
+  return orderDetail
+    ? res.status(200).json({
+        message: "Lấy thông tin chi tiết đơn hàng thành công!",
+        data: orderDetail,
+      })
+    : res.status(404).json({
+        message: "Không tìm thấy chi tiết đơn hàng với ID này",
+      });
 }
 
 export async function insertOrderDetail(req, res) {
-  res.status(200).json({
-    message: "thêm mới chi tiết đơn hàng thành công!",
+  const orderDetail = await db.orderDetail.create(req.body);
+  return res.status(201).json({
+    message: "Thêm mới chi tiết đơn hàng thành công!",
+    data: orderDetail,
   });
 }
 
 export async function deleteOrderDetail(req, res) {
-  res.status(200).json({
-    message: "xóa chi tiết đơn hàng thành công!",
+  const { id } = req.params;
+  const deleted = await db.orderDetail.destroy({
+    where: { id },
   });
+
+  return deleted
+    ? res.status(200).json({
+        message: "Xóa chi tiết đơn hàng thành công!",
+      })
+    : res.status(404).json({
+        message: "Không tìm thấy chi tiết đơn hàng với ID này",
+      });
 }
 
 export async function updateOrderDetail(req, res) {
-  res.status(200).json({
-    message: "cập nhật chi tiết đơn hàng thành công!",
+  const { id } = req.params;
+  const updatedOrderDetail = await db.orderDetail.update(req.body, {
+    where: { id },
   });
+
+  return updatedOrderDetail[0] > 0
+    ? res.status(200).json({
+        message: "Cập nhật chi tiết đơn hàng thành công!",
+      })
+    : res.status(404).json({
+        message: "Chi tiết đơn hàng không tìm thấy!",
+      });
 }
